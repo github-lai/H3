9c98f8b08c41c7faa5813151aa6cf8190fa5ecb4
github-lai <373515719@qq.com>
2020-04-25 05:29:25 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
-ini_set("short_open_tag","On");
-date_default_timezone_set('Asia/Shanghai');
+
 define("ROOTDIR",dirname(__FILE__)."/");
 define("ENV","dev");
-
-if(ENV != "live"){
-	error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
-}else{
-	error_reporting(0);//display nothing online
-}
 
 require 'vendor/autoload.php';
 
