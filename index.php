1c387438184daaeb033af1011f581c4a30cf5747
github-lai <373515719@qq.com>
2020-04-27 03:10:15 +0800

    pure index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-header("Content-type: text/html; charset=utf-8");
-
-define("ROOTDIR",dirname(__FILE__)."/");
-define("ENV","dev");
-
 require 'vendor/autoload.php';
-
-Lib\Loader::init();
 Lib\Router::dispatch();
-
 ?>
