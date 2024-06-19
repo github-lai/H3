632b36b4e093820276c1622a5a6b57a5b3a96caa
373515719@qq.com <373515719@qq.com>
2024-06-20 04:43:04 +0800

    删除测试文件

--- runtime.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function support($func)
-	{
-		echo function_exists($func) ? "<font color='green'>$func</font><br />":"<font color='red'>$func NOT SUPPORT</font><br />";
-	}
-	support('preg_match_all');
-?>
